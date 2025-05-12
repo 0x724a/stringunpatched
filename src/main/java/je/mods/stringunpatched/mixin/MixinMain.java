bc778d8ce214c5b02e762c002ede270a2c9e8a84
Jerry <a01472011@outlook.com>
2025-05-13 00:10:09 +0800

    fix

--- src/main/java/je/mods/stringunpatched/mixin/MixinMain.java
+++ src/main/java/je/mods/stringunpatched/mixin/MixinMain.java
@@ -34,16 +34,16 @@ public class MixinMain extends Block {
 
     @Inject(method = "calculateState", at = @At("HEAD"), cancellable = true)
     private void calculateState(Level arg, BlockPos arg2, BlockState arg3, boolean bl, boolean bl2, int l, @Nullable BlockState arg4, CallbackInfo ci) {
-        Direction direction = (Direction) arg3.getValue(FACING);
-        boolean flag = (Boolean) arg3.getValue(ATTACHED);
-        boolean flag1 = (Boolean) arg3.getValue(POWERED);
+        Direction direction = (Direction)arg3.getValue(FACING);
+        boolean flag = (Boolean)arg3.getValue(ATTACHED);
+        boolean flag1 = (Boolean)arg3.getValue(POWERED);
         boolean flag2 = !bl;
         boolean flag3 = false;
         int i = 0;
         BlockState[] ablockstate = new BlockState[42];
 
         BlockPos blockpos1;
-        for (int j = 1; j < 42; ++j) {
+        for(int j = 1; j < 42; ++j) {
             blockpos1 = arg2.relative(direction, j);
             BlockState blockstate = arg.getBlockState(blockpos1);
             if (blockstate.is(Blocks.TRIPWIRE_HOOK)) {
@@ -58,11 +58,11 @@ public class MixinMain extends Block {
                 flag2 = false;
             } else {
                 if (j == l) {
-                    blockstate = (BlockState) MoreObjects.firstNonNull(arg4, blockstate);
+                    blockstate = (BlockState)MoreObjects.firstNonNull(arg4, blockstate);
                 }
 
-                boolean flag4 = !(Boolean) blockstate.getValue(TripWireBlock.DISARMED);
-                boolean flag5 = (Boolean) blockstate.getValue(TripWireBlock.POWERED);
+                boolean flag4 = !(Boolean)blockstate.getValue(TripWireBlock.DISARMED);
+                boolean flag5 = (Boolean)blockstate.getValue(TripWireBlock.POWERED);
                 flag3 |= flag4 && flag5;
                 ablockstate[j] = blockstate;
                 if (j == l) {
@@ -74,53 +74,54 @@ public class MixinMain extends Block {
 
         flag2 &= i > 1;
         flag3 &= flag2;
-        BlockState blockstate1 = (BlockState) ((BlockState) this.defaultBlockState().setValue(ATTACHED, flag2)).setValue(POWERED, flag3);
+        BlockState blockstate1 = (BlockState)((BlockState)this.defaultBlockState().setValue(ATTACHED, flag2)).setValue(POWERED, flag3);
         if (i > 0) {
             blockpos1 = arg2.relative(direction, i);
             Direction direction1 = direction.getOpposite();
-            arg.setBlock(blockpos1, (BlockState) blockstate1.setValue(FACING, direction1), 3);
+            arg.setBlock(blockpos1, (BlockState)blockstate1.setValue(FACING, direction1), 3);
             this.notifyNeighbors(arg, blockpos1, direction1);
-            this.emitState(arg, blockpos1, flag2, flag3, flag, flag1);
+            this.playSound(arg, blockpos1, flag2, flag3, flag, flag1);
         }
 
-        this.emitState(arg, arg2, flag2, flag3, flag, flag1);
+        this.playSound(arg, arg2, flag2, flag3, flag, flag1);
         if (!bl) {
-            arg.setBlock(arg2, (BlockState) blockstate1.setValue(FACING, direction), 3);
+            arg.setBlock(arg2, (BlockState)blockstate1.setValue(FACING, direction), 3);
             if (bl2) {
                 this.notifyNeighbors(arg, arg2, direction);
             }
         }
 
         if (flag != flag2) {
-            for (int k = 1; k < i; ++k) {
+            for(int k = 1; k < i; ++k) {
                 BlockPos blockpos2 = arg2.relative(direction, k);
                 BlockState blockstate2 = ablockstate[k];
                 arg.setBlock(blockpos2, (BlockState)blockstate2.setValue(ATTACHED, flag2), 3);
             }
         }
+
         ci.cancel();
     }
-
-    @Shadow private void notifyNeighbors(Level arg, BlockPos arg2, Direction arg3) {
-        arg.updateNeighborsAt(arg2, this);
-        arg.updateNeighborsAt(arg2.relative(arg3.getOpposite()), this);
-    }
-    @Shadow private void emitState(Level arg, BlockPos arg2, boolean bl, boolean bl2, boolean bl3, boolean bl4) {
+    @Shadow
+    private void playSound(Level arg, BlockPos arg2, boolean bl, boolean bl2, boolean bl3, boolean bl4) {
         if (bl2 && !bl4) {
             arg.playSound((Player)null, arg2, SoundEvents.TRIPWIRE_CLICK_ON, SoundSource.BLOCKS, 0.4F, 0.6F);
-            arg.gameEvent((Entity)null, GameEvent.BLOCK_ACTIVATE, arg2);
+            arg.gameEvent(GameEvent.BLOCK_PRESS, arg2);
         } else if (!bl2 && bl4) {
             arg.playSound((Player)null, arg2, SoundEvents.TRIPWIRE_CLICK_OFF, SoundSource.BLOCKS, 0.4F, 0.5F);
-            arg.gameEvent((Entity)null, GameEvent.BLOCK_DEACTIVATE, arg2);
+            arg.gameEvent(GameEvent.BLOCK_UNPRESS, arg2);
         } else if (bl && !bl3) {
             arg.playSound((Player)null, arg2, SoundEvents.TRIPWIRE_ATTACH, SoundSource.BLOCKS, 0.4F, 0.7F);
-            arg.gameEvent((Entity)null, GameEvent.BLOCK_ATTACH, arg2);
+            arg.gameEvent(GameEvent.BLOCK_ATTACH, arg2);
         } else if (!bl && bl3) {
             arg.playSound((Player)null, arg2, SoundEvents.TRIPWIRE_DETACH, SoundSource.BLOCKS, 0.4F, 1.2F / (arg.random.nextFloat() * 0.2F + 0.9F));
-            arg.gameEvent((Entity)null, GameEvent.BLOCK_DETACH, arg2);
+            arg.gameEvent(GameEvent.BLOCK_DETACH, arg2);
         }
 
     }
-
+    @Shadow
+    private void notifyNeighbors(Level arg, BlockPos arg2, Direction arg3) {
+        arg.updateNeighborsAt(arg2, this);
+        arg.updateNeighborsAt(arg2.relative(arg3.getOpposite()), this);
+    }
 
 }

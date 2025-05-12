e0b6cedce585af14f9b2b356d71aa3995f614b56
Jerry <a01472011@outlook.com>
2025-05-12 23:53:20 +0800

    init

---
+++ src/main/java/je/mods/stringunpatched/ExampleMod.java
@@ -0,0 +1,14 @@
+package je.mods.stringunpatched;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ExampleMod {
+    public static final String MOD_ID = "stringunpatched";
+
+    public ExampleMod() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like registries and resources) may still be uninitialized.
+        // Proceed with mild caution.
+    }
+}
